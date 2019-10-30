7b41e8073dbee550b4dbd2dd132a62cb9b34811a
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-30 12:20:34 +0100

    console app e class library

---
+++ mistero3/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace mistero3
+{
+
+
+    public class Calcoli
+    {
+        public static int mistero2(int x, int y)
+        {
+
+            if (y == 0)
+            {
+                return 0;
+            }
+            else
+            {
+
+                return (mistero1(x, mistero2(x, y - 1)));
+            }
+
+        }
+
+        public static int mistero1(int x, int y)
+        {
+            int prec;
+            if (y == 0)
+            {
+                return x;
+            }
+            else
+            {
+                prec = mistero1(x, y - 1) + 1;
+                return prec;
+            }
+        }
+        public static int mistero3(int x, int y)
+        {
+            if (y == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return mistero2(x, mistero3(x, y - 1));
+            }
+        }
+    }
+}
---
+++ ricorsione/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using mistero3;
+
+namespace ricorsione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci un numero");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("inserisci un numero");
+            int y = int.Parse(Console.ReadLine());
+            double risultato = Calcoli.mistero3(x, y);
+            Console.WriteLine($"il risultato è {risultato}");
+            Console.ReadLine();
+        }
+    }
+}
